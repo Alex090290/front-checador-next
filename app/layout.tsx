10b27837126861e2a3f4bf8771f2dcde62c4eed5
Alex090290 <154947569+Alex090290@users.noreply.github.com>
2025-10-02 17:07:32 -0600

    limpieza de boilerplate e instalacion de paquetes

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import "bootswatch/dist/cosmo/bootstrap.css";
+import { Toaster } from "react-hot-toast";
+
+export const metadata: Metadata = {
+  title: "Checador Digital",
+  description: "Checador digital para GAMA",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body>
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
